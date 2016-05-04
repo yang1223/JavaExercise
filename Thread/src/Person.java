7761ccdb8dad83ceb4fea16dc67a6289df8f5fc5
Yang, Chi-Chang <whitesheep1223@gmail.com>
2016-05-04 20:45:11 +0800

    synchronized mehtod

--- Thread/src/Person.java
+++ Thread/src/Person.java
@@ -5,42 +5,86 @@ public class Person {
 
     private String name;
     private String gender;
+    private boolean sync = false;
 
-    public void setPerson(String name , String gender){
+    public void setSync(boolean sync){
+        this.sync = sync;
+    }
+
+    public void setPerson(String name , String gender) throws InterruptedException {
+        if(sync) syncSetPerson(name , gender);
+        else  asyncSetPerson(name , gender);
+    }
+
+
+    public synchronized void syncSetPerson (String name , String gender) throws InterruptedException {
         this.name = name;
+        Thread.sleep(10);
         this.gender = gender;
+        Thread.sleep(10);
         if(!check()) {
-            System.out.println("Not equal!");
+            System.out.println("Error!");
         } else {
-            System.out.println(".");
+            System.out.println("Fine...");
         }
     }
 
-    private boolean check(){
-        if (name.equals("Alice") && gender.equals("female")) return true;
-        else if (name.equals("Bob") && gender.equals("male")) return true;
-        else return false;
+    public void asyncSetPerson (String name , String gender) throws InterruptedException {
+        this.name = name;
+        Thread.sleep(10);
+        this.gender = gender;
+        Thread.sleep(10);
+        if(!check()) {
+            System.out.println("Error!");
+        } else {
+            System.out.println("Fine...");
+        }
+    }
+
+    private boolean check() {
+        return name.equals("Alice") && gender.equals("female")
+                || name.equals("Bob") && gender.equals("male");
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         final Person p = new Person();
 
-        Thread t1 = new Thread(new Runnable() {
+        p.setSync(false);
+        System.out.println("==================");
+        System.out.println("Asynchronized. There may be some error with name and gender!");
+        Thread t1 = getNewThread("Alice" , "female" , p);
+        Thread t2 = getNewThread("Bob" , "male" , p);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        p.setSync(true);
+        System.out.println("==================");
+        System.out.println("Synchronized. There must be 'Fine...' every time. ");
+        t1 = getNewThread("Alice" , "female" , p);
+        t2 = getNewThread("Bob" , "male" , p);
+        t1.start();
+        t2.start();
+
+    }
+
+    public static Thread getNewThread(final String name , final String gender , final Person p){
+        return new Thread(new Runnable() {
             @Override
             public void run() {
-                while(true) p.setPerson("Alice" , "female");
+                for(int i = 0 ; i < 5 ; i++ ) {
+                    try {
+                        p.setPerson(name , gender);
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
-
-
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true) p.setPerson("Bob" , "male");
-            }
-        });
-
     }
 
 
